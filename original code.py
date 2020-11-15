981da19a5bea14963efbc661f1f281659d59ac4f
BigDom32542888 <74510011+BigDom32542888@users.noreply.github.com>
2020-11-15 14:01:32 -0600

    Add files via upload

---
+++ original code.py
@@ -0,0 +1,15 @@
+N = input("Please pick an integer N to make a NxN Board:\n")
+
+class NRooks:
+
+    def initialBoard(self, size):
+        size = N
+        self.size = size
+        self.restart()
+
+    def restart(self):
+        size = self.size
+        self.row = [None]*size
+        self.
+    
+        
