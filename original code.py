215972bbe75b4d95a7188cf60ed5dc54941ecb99
BigDom32542888 <74510011+BigDom32542888@users.noreply.github.com>
2020-11-15 22:32:28 -0600

    Update original code.py

--- original code.py
+++ original code.py
@@ -1,15 +1,20 @@
 N = input("Please pick an integer N to make a NxN Board:\n")
 
-class NRooks:
+class NAdjustedKing:
 
+    "the board size and number of solutions"
     def initialBoard(self, size):
         size = N
         self.size = size
-        self.restart()
-
-    def restart(self):
-        size = self.size
-        self.row = [None]*size
-        self.
+        self.solutions = 0
+        self.solve()
+        
+    "solve the problem and print num solutions"
+    def solve(self):
+        positions = [-1] * self.size
+        self.place_king(positions, 0)
+        print("Found", self.solutions, "solutions!")
+        
+    "Place the king on a row and check N cases.  If a valid solutuon
     
         
