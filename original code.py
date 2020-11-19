13f01ccdb26caa21c3c57299d828fd89404d0bf3
BigDom32542888 <74510011+BigDom32542888@users.noreply.github.com>
2020-11-19 11:44:08 -0600

    Update original code.py

--- original code.py
+++ original code.py
@@ -7,8 +7,14 @@ print("The Chess Board You Have Selected Is A", N,"x",M, "Board!")
 class NAdjustedKing:
 
     "the board size and number of solutions"
+    #Dont know how to get board to work
     def initialBoard(row, column):
-        matrix
+        row = M
+        col = N
+        size = 
+        self.size = size
+        self.solutions = 0
+        self.solve()
         
     "solve the problem and print num solutions"
     def solve(self):
@@ -17,15 +23,17 @@ class NAdjustedKing:
         print("Found", self.solutions, "solutions!")
         
     "Place the king on a row and check N cases.  If a valid solutuon
-    
-    def put_king(self, positions, target_row):
-        for row in range(1:M):
-            if row = M:
-                self.show_full_board(positions)
-                self.solutuions += 1
-            else:
-                for col in range(1:N):
-                    if col = N:
-                        return;
+    #Cant get the king to work
+    def put_king(self, positions, target_row, taget_col):
+        for target_row in range(1:M):
+            if target_row = M:
+                for target_col in range(1:N):
+                    if target_col = N:
+                        self.show_full_board(positions)
+                        self.solutuions += 1
                     else:
-                        posput_king
+                        for target_col in range(1:N):
+                            if col = N:
+                                return;
+                    else:
+                        
